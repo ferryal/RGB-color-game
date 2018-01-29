a5046cd8b81a0abb6493476325cf8f3258d9f45a
ferryal <Ferryal21@gmail.com>
2018-01-29 23:00:56 +0700

    update logic inde.js

--- index.js
+++ index.js
@@ -9,16 +9,24 @@ let color = [
 let square= document.querySelectorAll(".square");
 let pickedColor= color[3];
 let colorDisplay=document.getElementById("colorDisplay");
+let message =document.querySelector('#message');
 
 colorDisplay.textContent= pickedColor
 
- function getRandomInt(min, max){
+let getRandomInt= (min, max) => {
   return Math.floor(Math.random()* (max-min+1)) + min;
 }
+//
+// let setInterval = () => {
+//   let colors = document.querySelector('#square');
+//   let r = getRandomInt(0,255);
+//   let g = getRandomInt(0,255);
+//   let b = getRandomInt(0,255);
+//
+//   colors.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+//   document.getElementById("colorDisplay").innerHTML =`${r}, ${g}, ${b}`;
+// }
 
-setInteval(function(){
-  
-})
 for (var i = 0; i < square.length; i++) {
   square[i].style.background = color[i];
 
@@ -26,7 +34,18 @@ for (var i = 0; i < square.length; i++) {
 
     let colorPicked=this.style.background;
     if (colorPicked === pickedColor){
-        alert('correct');
+        message.textContent = "Correct";
+        changeColor(colorPicked);
+    } else {
+      this.style.background = "#232323";
+      message.textContent = "Try Again";
     }
   });
 }
+
+//
+// let changeColor =  (color) => {
+//   for (var i = 0; i < color.length; i++) {
+//     color[i].style.background =color;
+//   }
+// }
