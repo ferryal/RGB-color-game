4bb7d82aebb0929c23dfad7fe039fc9120f8edbe
ferryal <Ferryal21@gmail.com>
2018-01-29 09:36:45 +0700

    add logic random generator

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ let color = [
   "rgb(0, 255, 255)",
   "rgb(0, 255, 0)",
 ]
+let square= document.querySelectorAll(".square");
+let pickedColor= color[3];
+let colorDisplay=document.getElementById("colorDisplay");
 
-let square= document.querySelectorAll('.square');
+colorDisplay.textContent= pickedColor
 
+ function getRandomInt(min, max){
+  return Math.floor(Math.random()* (max-min+1)) + min;
+}
+
+setInteval(function(){
+  
+})
 for (var i = 0; i < square.length; i++) {
   square[i].style.background = color[i];
+
+  square[i].addEventListener('click', function() {
+
+    let colorPicked=this.style.background;
+    if (colorPicked === pickedColor){
+        alert('correct');
+    }
+  });
 }
